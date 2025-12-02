da0300808d07f21d760a47ff1435650762eac9dd
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-12-02 13:38:53 +0000

    fix: cleaning input

--- isPalindrome.ts
+++ isPalindrome.ts
@@ -1,7 +1,7 @@
 const isPallindrome = (input: string) => {
   console.log("function isPallindrome");
   const lowercase = input.toLocaleLowerCase();
-  const cleaned = lowercase.replace(/[^a-z0-9\s]/g, "");
+  const cleaned = lowercase.replace(/[^a-z0-9]/g, "");
   const array = cleaned.split("");
   const reversedArray = array.reverse();
   const reversedString = reversedArray.join("");
