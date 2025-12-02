8e49a4e5d52bcf56fedbf48e0f39089d4fa8b471
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-12-02 13:47:04 +0000

    refactor: test in own folder

--- test/unit/isPalindrome.test.js
+++ test/unit/isPalindrome.test.js
@@ -1,6 +1,6 @@
 import { it, describe } from "node:test";
 import assert from "node:assert";
-import isPallindrome from "./isPalindrome.ts";
+import isPallindrome from "../../isPalindrome.ts";
 
 describe("test happy path", () => {
   it("should return true for 'a', a single alphanumeric characters", () => {
