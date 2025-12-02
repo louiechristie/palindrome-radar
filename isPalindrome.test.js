f2aae3b9d828363fab4b103984d79eee18f4cea0
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-12-02 12:48:27 +0000

    test: mulitple characters

--- isPalindrome.test.js
+++ isPalindrome.test.js
@@ -2,8 +2,20 @@ import { it, describe } from "node:test";
 import assert from "node:assert";
 import isPallindrome from "./isPalindrome.ts";
 
-describe("test", () => {
-    it("should return true for 'a', a single alphanumeric characters", () => {
-        assert.equal(isPallindrome("a"), true);
-    });
+describe("test happy path", () => {
+  it("should return true for 'a', a single alphanumeric characters", () => {
+    assert.equal(isPallindrome("a"), true);
+  });
+
+  it("should return true for 'aa'", () => {
+    assert.equal(isPallindrome("aa"), true);
+  });
+
+  it("should return true for 'aa'", () => {
+    assert.equal(isPallindrome("aa"), true);
+  });
+
+  it("should return true for 'aaa'", () => {
+    assert.equal(isPallindrome("aaa"), true);
+  });
 });
