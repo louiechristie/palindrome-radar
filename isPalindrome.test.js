99e989bd1fdc2ba795fc96b1aab84175499abfb2
Louie Christie <6807448+louiechristie@users.noreply.github.com>
2025-12-02 13:41:30 +0000

    test: with numbers

--- isPalindrome.test.js
+++ isPalindrome.test.js
@@ -26,6 +26,12 @@ describe("test happy path", () => {
   it("should return true for 'anina' with odd letter in middle", () => {
     assert.equal(isPallindrome("anina"), true);
   });
+
+  describe("test with numbers", () => {
+    it("should return true for '1'", () => {
+      assert.equal(isPallindrome("1"), true);
+    });
+  });
 });
 
 describe("test un-happy path", () => {
@@ -35,6 +41,12 @@ describe("test un-happy path", () => {
 
   it("should return false for 'abbacus'", () => {
     assert.equal(isPallindrome("abbacus"), false);
+  });
+
+  describe("test with numbers", () => {
+    it("should return false for 'a1'", () => {
+      assert.equal(isPallindrome("a1"), false);
+    });
   });
 });
 
